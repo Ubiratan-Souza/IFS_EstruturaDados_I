b74a4e601446ba0c7b6defc7b370d5f0d7dabbb2
Ubiratan-Souza <ubiratan.souza@protonmail.com>
2021-02-08 12:50:24 -0300

    Correção subclasse.

--- src/ED_Listas/Lista.java
+++ src/ED_Listas/Lista.java
@@ -3,11 +3,11 @@ package ED_Listas;
 public abstract class Lista <T extends Comparable>{
     public void Lista(){
     }
-    public abstract void incluir (T elemento) thows Exception;
+    public abstract void incluir (T elemento) throws Exception;
     public abstract void incluirInicio(T elemento) throws Exceptiom;
     public abstract void incluir (T elemento, int posicao) throws Exception;
     public abstract T get (int posicao) throws Exception;
-    public abstract void remover (int posicao) thwos Exception;
+    public abstract void remover (int posicao) throws Exception;
     public abstract void limpar();
     public abstract int getTamanho();
     public abstract boolean contem (T elemento) throws Exception;
