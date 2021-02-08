17dd033a6e5bdf7813ffbe1006b2320a429b0641
Ubiratan-Souza <ubiratan.souza@protonmail.com>
2021-02-08 12:48:04 -0300

    Subclasse requisitada pelo professor.

--- src/ED_Listas/Lista.java
+++ src/ED_Listas/Lista.java
@@ -1,5 +1,15 @@
 package ED_Listas;
 
-public class Lista {
+public abstract class Lista <T extends Comparable>{
+    public void Lista(){
+    }
+    public abstract void incluir (T elemento) thows Exception;
+    public abstract void incluirInicio(T elemento) throws Exceptiom;
+    public abstract void incluir (T elemento, int posicao) throws Exception;
+    public abstract T get (int posicao) throws Exception;
+    public abstract void remover (int posicao) thwos Exception;
+    public abstract void limpar();
+    public abstract int getTamanho();
+    public abstract boolean contem (T elemento) throws Exception;
 
 }
